236b9d4cfaffcf1c286c3e0ceddd131c463ebed1
sidd190 <73955358+sidd190@users.noreply.github.com>
2026-01-09 14:18:52 +0530

    Update constants.js

--- libs/constants.js
+++ libs/constants.js
@@ -1,6 +1,6 @@
 
 export const IS_EDITABLE = window.location.search.includes('edit=true')
-export const OWNER = "YOUR-USERNAME";
+export const OWNER = "sidd190";
 export const REPO = "MY-GSOC-TOOL";
-export const EMAIL = "your.email@example.com"
+export const EMAIL = "sidd190bansal@gmail.com"
 export const BRANCH = "main"
